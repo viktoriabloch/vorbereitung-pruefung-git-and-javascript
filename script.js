1c6341f35ce169dbc2e8983bac548ffb232e8a3b
viktoriabloch <e247769@sbl.ch>
2022-12-07 18:38:14 +0100

    bounce hinzugefügt

--- script.js
+++ script.js
@@ -8,6 +8,9 @@ function start() {
 function update() {
     dy = dy + 0.5
 
+    if (cy > 100)
+    dy = dy * (-1)
+    
     let cy = parseFloat(circle.getAttribute("cy"))
     cy = cy + dy
     circle.setAttribute ("cy", cy)
